b19f1f35e9c5c407e2396cd0eb648aa6860bfdec
Carlos Abade <carloseabade@gmail.com>
2023-08-25 22:14:12 -0300

    refactor: organization

--- server.go
+++ server.go
@@ -31,23 +31,27 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
   if r.Method == "POST" {
     r.ParseMultipartForm(1 << 20)
+
     uploadedFile, handler, err := r.FormFile("uploadFile")
     if err != nil {
       log.Fatal(err)
       return
     }
     defer uploadedFile.Close()
+
     localFile, err := os.OpenFile("./sub.ass", os.O_WRONLY|os.O_CREATE, 0666)
     if err != nil {
       log.Fatal(err)
       return
     }
     defer localFile.Close()
+
     _, err = io.Copy(localFile, uploadedFile)
     if err != nil {
       log.Fatal(err)
       return
     }
+
     dialogues := extractDialogue("sub.ass")
     dialogueFile, err := os.OpenFile("./d"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
     if err != nil {
@@ -55,14 +59,17 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
       return
     }
     defer dialogueFile.Close()
+
     _, err = dialogueFile.WriteString(dialogues)
     if err != nil {
       log.Fatal(err)
       return
     }
+
     w.Header().Set("Content-Disposition", "attachment; filename=d"+handler.Filename)
     w.Header().Set("Content-Type", "application/octet-stream")
     http.ServeFile(w, r, "d"+handler.Filename)
+
     err = os.Remove("d"+handler.Filename)
     if err != nil {
       log.Fatal(err)
@@ -78,3 +85,4 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte(fmt.Sprintf("{\"error\":\"method not allowed\"}")))
   }
 }
+
